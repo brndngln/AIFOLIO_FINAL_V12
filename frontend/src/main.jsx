7ab7e0de43cfc1eabac3d15f31dd07f62c265608
Brendan Smith <brengsmith5@gmail.com>
2025-06-22 19:19:24 -0600

    commit changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import ThemeProvider from '../theme/ThemeProvider.jsx'
 import App from './App'
+import { register as registerServiceWorker } from '../registerServiceWorker'
+
+registerServiceWorker()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
